f3b42611a0a42e0aa3a2fea1d7abd3dbca77d8e9
gagan gupta <gagan202020@gmail.com>
2025-01-24 18:24:23 +0530

    grouping by clauses examples

--- src/main/java/com/streams/Main.java
+++ src/main/java/com/streams/Main.java
@@ -1,6 +1,8 @@
 package com.streams;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 class Pair{
@@ -31,13 +33,39 @@ class Pair{
 }
 class Data{
 
-    private final String name;
-    private final Integer id;
-    private final int size;
+    private String name;
+    private Integer id;
+    private int size;
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", size=" + size +
+                '}';
+    }
+
+    public Data() {
+        id = 0;
+        size = 0;
+    }
 
     public Data(String name, Integer id, int size) {
         this.name = name;
         this.id = id;
+        this.size = size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setSize(int size) {
         this.size = size;
     }
 
@@ -51,6 +79,12 @@ class Data{
 
     public int getSize() {
         return size;
+    }
+
+    public Data merge(Data other) {
+        this.size += other.getSize();
+        this.id += other.getId();
+        return this;
     }
 
     @Override
@@ -74,30 +108,53 @@ public class Main {
         List<Data> dataList = new ArrayList<>();
         dataList.add(new Data("1", 1, 1));
         dataList.add(new Data("1", 11, 11));
-        dataList.add(new Data("1", 11, 11));
+        dataList.add(new Data("1", 12, 12));
         dataList.add(new Data("2", 2, 2));
         dataList.add(new Data("3", 3, 3));
 
-        Map<String, List<Data>> mapByNameAsKey = dataList.stream().collect(Collectors.groupingBy(Data::getName));
-        for (String string : mapByNameAsKey.keySet()){
-            System.out.println("key : " + string + " list : ");
-            mapByNameAsKey.get(string).stream().forEach((a) -> System.out.println(a.getId()));
+        System.out.println("++++++++map filtering++++++++++");
+        Map<String, Long> mapFiltering = dataList.stream().collect(Collectors.groupingBy(
+                a -> a.getName(),
+                Collectors.counting()));
+        for (String string : mapFiltering.keySet()){
+            System.out.println("key : " + string + " count : " + mapFiltering.get(string));
         }
-        System.out.println("++++++++++++++++++");
 
-        Map<String, Set<Data>> mapToSet = dataList.stream().collect(Collectors.groupingBy(Data::getName, Collectors.toSet()));
-        for (String string : mapToSet.keySet()){
-            System.out.println("key : " + string + " set : ");
-            mapToSet.get(string).stream().forEach((a) -> System.out.println(a.getId()));
-        }
-        System.out.println("++++++++++++++++++");
-
+        System.out.println("++++++++counting++++++++++");
         Map<String, Long> mapToCount = dataList.stream().collect(Collectors.groupingBy(Data::getName, Collectors.counting()));
         for (String string : mapToCount.keySet()){
             System.out.println("key : " + string + " count : " + mapToCount.get(string));
         }
-        System.out.println("++++++++++++++++++");
 
+        System.out.println("++++++++mapToMax++++++++++");
+        Map<String, Optional<Data>> mapToMax = dataList
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Data::getName,
+                                Collectors.reducing(
+                                        BinaryOperator.maxBy(
+                                                Comparator.comparing(Data::getSize)
+                                        )
+                                )));
+        for (String string : mapToMax.keySet()){
+            System.out.println("key : " + string + " mapToMax : " + mapToMax.get(string).orElseGet(null));
+        }
+
+        System.out.println("+++++++++mapToSum+++++++++");
+        Map<String, Data> mapToSum = dataList.stream().collect(Collectors.groupingBy(
+                Data::getName,
+                Collector.of(
+                        Data::new,
+                        Data::merge,
+                        Data::merge
+                        )
+        ));
+        for (String string : mapToSum.keySet()){
+            System.out.println("key : " + string + " mapToSum : " + mapToSum.get(string));
+        }
+
+        System.out.println("++++++++mapToCustomList++++++++++");
         Map<String, List<Integer>> mapToCustomList = dataList
                 .stream()
                 .collect(Collectors
@@ -107,15 +164,15 @@ public class Main {
             System.out.println("key : " + string + " custom : ");
             System.out.println(mapToCustomList.get(string).stream().map(a -> String.valueOf(a)).reduce((a,b) -> a + "_" + b).get());
         }
-        System.out.println("++++++++++++++++++");
 
+        System.out.println("++++++++mapByPair++++++++++");
         Map<Pair, List<Data>> mapByPair = dataList.stream().collect(Collectors.groupingBy(data -> new Pair(data.getName(), data.getId())));
         for (Pair pair : mapByPair.keySet()){
             System.out.println("key : " + pair.getName() + " : " + pair.getId() + " pair value : " + mapByPair.get(pair));
             mapByPair.get(pair).stream().forEach((a) -> System.out.println(a.getId()));
         }
-        System.out.println("++++++++++++++++++");
 
+        System.out.println("++++++++ mapByNewData as key ++++++++++");
         Map<Data, List<Data>> mapByNewData =
                 dataList.stream().collect(Collectors.groupingBy(data -> new Data(data.getName(), data.getId(), data.getSize())));
         for (Data data : mapByNewData.keySet()){
@@ -123,7 +180,6 @@ public class Main {
             mapByNewData.get(data).stream().forEach((a) -> System.out.println(a.getId()));
         }
         System.out.println("++++++++++++++++++");
-
 
     }
 
